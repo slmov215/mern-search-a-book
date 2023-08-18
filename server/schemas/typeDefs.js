c1d591ff0a1b9ada24c119998db28ed2c0774917
Stephen Mov <slmov215@gmail.com>
2023-08-18 18:44:55 -0400

    Worked on server side , typedefs and revolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,17 +1,43 @@
+const { gql } = require("apollo-server-express");
 
 const typeDefs = `
 type User {
-  _id: ID!
-  username: String!
+  _id: ID
+  username: String
+  email: String
+  bookCount: Int
+  savedBooks: [Book]
 }
-
+type Book {
+  bookId: ID
+  authors: [String]
+  description: String
+  title: String
+  image: String
+  link: String
+}
 type Query {
-  users: [User]!
-  user(userId: ID!): User
+  me: User
+  users: [User]
+  user(username: String!): User
 }
-
+input BookInput {
+  authors: [String]
+  description: String
+  bookId: String!
+  image: String
+  link: String
+  title: String!
+}
 type Mutation {
-  addUser(name: String!): User
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  saveBook(bookInput: BookInput): User
+  removeBook(bookId: ID!): User
+}
+type Auth {
+  token: ID!
+  user: User
 }
 `
 
