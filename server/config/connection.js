b8ca08beb35caed37e398c6175f9c120bc0b7236
Stephen <127278005+slmov215@users.noreply.github.com>
2023-08-20 01:17:10 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-serach-a-book');
 
 module.exports = mongoose.connection;
